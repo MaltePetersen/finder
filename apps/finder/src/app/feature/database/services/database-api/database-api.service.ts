dbc86738d4d37fa6b9d9b67860e9f85ba501aa76
Malte Petersen <mp@marketoolz.com>
2021-01-30 20:04:52 +0100

    Unötigen Code entfernt

--- apps/finder/src/app/feature/database/services/database-api/database-api.service.ts
+++ apps/finder/src/app/feature/database/services/database-api/database-api.service.ts
@@ -8,43 +8,33 @@ import { Observable } from 'rxjs';
   providedIn: 'root',
 })
 export class DatabaseApiService {
-  path = (path: string) => `api/database-handling/${path}`;
-
   constructor(private http: HttpClient, private apiService: ApiService) {}
 
   getAllCollections() {
     return this.http.get(this.apiService.url(ApiEndpoints.API_GET_COLLECTIONS));
-    return this.http.get(this.path('collections'));
   }
   getCollection(colname: string): Observable<Array<any>> {
     return this.http.get<[]>(this.apiService.url(ApiEndpoints.API_GET_COLLECTION, { colname: colname }));
-    return this.http.get<[]>(this.path(`collection:${colname}`));
   }
   createCollection(colname: string) {
     return this.http.post(this.apiService.url(ApiEndpoints.API_POST_COLLECTION), { name: colname });
-    return this.http.post(this.path(`collection`), { name: colname });
   }
   deleteCollection(colname: string) {
     return this.http.delete(this.apiService.url(ApiEndpoints.API_DELETE_COLLECTION, { colname: colname }));
-    return this.http.delete(this.path(`collection:${colname}`));
   }
   updateCollection(colname: string, newColName: string) {
     return this.http.put(this.apiService.url(ApiEndpoints.API_PUT_COLLECTION), { name: colname, newName: newColName });
-    return this.http.put(this.path(`collection`), { name: colname, newName: newColName });
   }
 
   createEntry(entry: Object, colname: string) {
     return this.http.post(this.apiService.url(ApiEndpoints.API_POST_ENTRY, { colname: colname }), entry);
-    return this.http.post(this.path(`collection:${colname}/entry`), entry);
   }
 
   updateEntry(entry: Object, colname: string, id: string) {
     return this.http.put(this.apiService.url(ApiEndpoints.API_PUT_ENTRY, { colname: colname, id: id }), entry);
-    return this.http.put(this.path(`collection:${colname}/entry:${id}`), entry);
   }
 
   deleteEntry(colname: string, id: string) {
     return this.http.delete(this.apiService.url(ApiEndpoints.API_DELETE_ENTRY, { colname: colname, id: id }));
-    return this.http.delete(this.path(`collection:${colname}/entry:${id}`));
   }
 }
--- apps/finder/src/app/feature/file-manager/services/file-manager-api/file-manager-api.service.ts
+++ apps/finder/src/app/feature/file-manager/services/file-manager-api/file-manager-api.service.ts
@@ -19,27 +19,23 @@ export class FileManagerApiService {
 
   public getFileNode() {
     return this.http.get<FileNode[]>(this.apiService.url(ApiEndpoints.API_GET_DIR_FILENODE));
-    return this.http.get<FileNode[]>('/api/directory/filenode');
   }
   getStatsOfFile(path: string) {
-    path = this.transformSlashes(path);
-    return this.http.get<Stats<any>>(this.apiService.url(ApiEndpoints.API_GET_STATS, { path: path }));
-    return this.http.get<Stats<any>>(`/api/file/stats:${path}`);
+    return this.http.get<Stats<any>>(
+      this.apiService.url(ApiEndpoints.API_GET_STATS, { path: this.transformSlashes(path) })
+    );
   }
   transformSlashes(path: string) {
     return path.replace(/\//g, '%2F');
   }
   readFile(path: string) {
     return this.http.get<any>(this.apiService.url(ApiEndpoints.API_GET_FILE, { path: this.transformSlashes(path) }));
-    return this.http.get<any>(`/api/file/:${this.transformSlashes(path)}`);
   }
   deleteFile(path: string) {
     return this.http.delete(this.apiService.url(ApiEndpoints.API_DELETE_FILE, { path: this.transformSlashes(path) }));
-    return this.http.delete(`/api/file/:${this.transformSlashes(path)}`);
   }
   createFile(fileDTO: FileDTO) {
     return this.http.post(this.apiService.url(ApiEndpoints.API_POST_FILE), fileDTO);
-    return this.http.post('/api/file', fileDTO);
   }
   copyFile(fromPath: string, toPath: string) {
     return this.http.get(
@@ -48,24 +44,18 @@ export class FileManagerApiService {
         to: this.transformSlashes(toPath),
       })
     );
-    return this.http.get(`/api/file/copy:${this.transformSlashes(fromPath)}:${this.transformSlashes(toPath)}`);
   }
   updateFileName(updateFileDTO: UpdateFileDTO) {
     return this.http.put(this.apiService.url(ApiEndpoints.API_PUT_FILE), updateFileDTO);
-    return this.http.put('/api/file', updateFileDTO);
   }
   createDirectory(directoryDTO: DirectoryDTO) {
     return this.http.post(this.apiService.url(ApiEndpoints.API_POST_DIR), directoryDTO);
-    return this.http.post('/api/directory', directoryDTO);
   }
   updateDirectory(updateDirectoryDTO: UpdateDirectoryDTO) {
     return this.http.put(this.apiService.url(ApiEndpoints.API_PUT_DIR), updateDirectoryDTO);
-    return this.http.put('/api/directory', updateDirectoryDTO);
   }
   deleteDirectory(path: string) {
-    path = this.transformSlashes(path);
-    return this.http.delete(this.apiService.url(ApiEndpoints.API_DELETE_DIR, { path: path }));
-    return this.http.delete('/api/directory/:' + path);
+    return this.http.delete(this.apiService.url(ApiEndpoints.API_DELETE_DIR, { path: this.transformSlashes(path) }));
   }
   copyDirectory(fromPath: string, toPath: string) {
     return this.http.get(
@@ -74,10 +64,8 @@ export class FileManagerApiService {
         to: this.transformSlashes(toPath),
       })
     );
-    return this.http.get(`/api/directory/copy:${this.transformSlashes(fromPath)}:${this.transformSlashes(toPath)}`);
   }
   getWorkspace() {
     return this.http.get(this.apiService.url(ApiEndpoints.API_GET_WORKSPACE));
-    return this.http.get('/api/directory/workspace');
   }
 }
