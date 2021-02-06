051aa19fa215eacb66305674ad214e189b0ee55e
Malte Petersen <mp@marketoolz.com>
2021-02-06 13:58:57 +0100

    LoginGuard hinzugefuegt

--- apps/finder/src/app/app-routing.module.ts
+++ apps/finder/src/app/app-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { AuthGuard } from './shared/login/auth.guard';
+import { LoginGuard } from './shared/login/login.guard';
 import { LoginComponent } from './shared/login/login/login.component';
 
 const routes: Routes = [
@@ -14,7 +15,8 @@ const routes: Routes = [
     canActivate: [AuthGuard],
     loadChildren: () => import('./feature/database/database.module').then((m) => m.DatabaseModule),
   },
-  { path: 'login', component: LoginComponent },
+  { path: 'login', canActivate: [LoginGuard], component: LoginComponent },
+  { path: '**', redirectTo: '/file-manager' },
 ];
 
 @NgModule({
---
+++ apps/finder/src/app/shared/login/login.guard.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree, Router } from '@angular/router';
+import { Observable } from 'rxjs';
+import { AuthService } from '../services/auth/auth.service';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class LoginGuard implements CanActivate {
+  constructor(private authService: AuthService, private router: Router) {}
+  canActivate(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot
+  ): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+    if (this.authService.isAuthenticated()) {
+      this.router.navigate(['/file-manager']);
+      return false;
+    }
+    return true;
+  }
+}
