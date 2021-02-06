d6f6606002698c626545614a4376b8d15f501b55
Malte Petersen <mp@marketoolz.com>
2021-02-06 13:17:38 +0100

    Local storage hinzugefuegt komplettes Beispiel Arbeit johanna

--- apps/finder/src/app/shared/services/auth/auth.service.ts
+++ apps/finder/src/app/shared/services/auth/auth.service.ts
@@ -8,16 +8,25 @@ export class AuthService {
   fakeBackenddata = [{ name: 'Malte', password: '12345' }];
   private readonly user$$ = new BehaviorSubject<User | null>(null);
   public readonly user$ = this.user$$.asObservable();
-
+  constructor() {
+    const user = localStorage.getItem('user');
+    if (user) {
+      this.login(JSON.parse(user));
+    }
+  }
   login(user: User) {
     if (
       this.fakeBackenddata.some((fakeBackendDataUser) => JSON.stringify(fakeBackendDataUser) === JSON.stringify(user))
     ) {
       this.user$$.next(user);
+      localStorage.setItem('user', JSON.stringify(user));
       return true;
     }
     this.user$$.next(null);
     return false;
+  }
+  logout() {
+    localStorage.clear();
   }
   isAuthenticated() {
     return this.user$$.value === null ? false : true;
