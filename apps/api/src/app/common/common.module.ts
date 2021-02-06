25700254acccc431bc47fbd11e8bcaf1b2b72722
Malte Petersen <mp@marketoolz.com>
2021-02-06 15:58:41 +0100

    Backend authentication works local + jwt

--- apps/api/src/app/common/common.module.ts
+++ apps/api/src/app/common/common.module.ts
@@ -1,10 +1,21 @@
 import { Global, Module } from '@nestjs/common';
+import { APP_GUARD } from '@nestjs/core';
+import { AuthModule } from './auth/auth.module';
+import { JwtAuthGuard } from './auth/jwt-auth.guard';
+import { AppController } from './login/app.controller';
 import { SharedDirectoryService } from './service/shared-directory/shared-directory.service';
+import { UsersModule } from './users/users.module';
 @Global()
 @Module({
-  imports: [],
-  controllers: [],
-  providers: [SharedDirectoryService],
+  imports: [UsersModule, AuthModule],
+  controllers: [AppController],
+  providers: [
+    SharedDirectoryService,
+    {
+      provide: APP_GUARD,
+      useClass: JwtAuthGuard,
+    },
+  ],
   exports: [SharedDirectoryService],
 })
 export class CommonModule {}
