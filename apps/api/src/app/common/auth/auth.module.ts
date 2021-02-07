f54cbef10aa89f271b2e3c28c257a194d4aa544a
Malte Petersen <mp@marketoolz.com>
2021-02-07 13:31:04 +0100

    JWT tokens do not expire now

--- apps/api/src/app/common/auth/auth.module.ts
+++ apps/api/src/app/common/auth/auth.module.ts
@@ -13,7 +13,6 @@ import { jwtConstants } from './constants';
     PassportModule,
     JwtModule.register({
       secret: jwtConstants.secret,
-      signOptions: { expiresIn: '60s' },
     }),
   ],
   providers: [AuthService, LocalStrategy, JwtStrategy],
